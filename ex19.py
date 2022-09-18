b24398087040fe50cd0e508a94c909814b9dcdaa
olegmonogarov <107205601+Oleg-mng@users.noreply.github.com>
2022-09-18 13:27:49 +0300

    Update ex19.py

--- ex19.py
+++ ex19.py
@@ -4,11 +4,19 @@
 # в файл многочлен степени k.
 # Пример:
 # k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
 import random
-mylist=[]
 k = int(input('Введите натуральную степень k: '))
+mylist = []
 for i in range(k+1):
     a = random.randint(0, 100)
-    print(a, 'x^', i)
-# with open('file19.txt', 'w') as data:
-#     data.write('mylist')
+    b = a, 'x^', i
+    b = ''.join(map(str, b))
+    # print(b)
+    mylist.append(b)
+mylist.reverse()
+# print(mylist)
+print('+'.join(map(str, mylist)))
+g = '+'.join(map(str, mylist))
+with open('file19.txt', 'w') as data:
+    data.write(g)
