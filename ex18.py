a8fc80e395e74313bff145a88103876d9f714d4e
olegmonogarov <107205601+Oleg-mng@users.noreply.github.com>
2022-09-18 12:31:05 +0300

    Update ex18.py

--- ex18.py
+++ ex18.py
@@ -4,7 +4,19 @@
 # последовательности.
 # [1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
 
-mylist = [1, 1, 2, 3, 4, 5, 5]
+def original_numbers (list):
+    helplist = []
+    for i in mylist:
+        count = 0
+        for j in mylist:
+            if i == j:
+                count += 1
+        if count == 1:
+            helplist.append(i)
+    print(helplist)
+
+mylist = [1, 1, 2, 8, 3, 4, 5, 5, 6, 8, 8, 8]
+original_numbers(mylist)
 
 # for i in range(len(mylist)):
 #     for j in range(len(mylist)):
@@ -13,12 +25,10 @@ mylist = [1, 1, 2, 3, 4, 5, 5]
 #             mylist.remove(mylist[j])
 # print(mylist)
 
-helplist=mylist
-for i in mylist:
-    for j in mylist:
-        if i == j:
-            helplist.remove(j)
-            helplist.remove(j)
-print(helplist)
-
-
+# helplist=mylist
+# for i in mylist:
+#     for j in mylist:
+#         if i == j:
+#             helplist.remove(j)
+#             helplist.remove(j)
+# print(helplist)
