139925018dc20222e744ace86b18384aa3f46e63
olegmonogarov <107205601+Oleg-mng@users.noreply.github.com>
2022-09-18 14:18:19 +0300

    Update ex20.py

--- ex20.py
+++ ex20.py
@@ -4,6 +4,21 @@
 
 # from solution import Polynomial
 
+def numbers_in_list(list):
+    sum_numbers_1 = []
+    for i in list:
+        if i.isdigit() == True:
+            sum_numbers_1.append(i)
+    print(sum_numbers_1)
+    return sum_numbers_1
+    
+
+# def sum_of_lists(list1, list2):
+#     sum_of_lists_1 = []
+#     for i in list1:
+#         sum_of_lists_1=list1+list2
+
+
 with open('file20.txt', 'w') as data:
     data.write('3*x^2+7*x+9')
 with open('file20.1.txt', 'w') as data:
@@ -17,21 +32,26 @@ with open('file20.1.txt', 'r') as f:
     poly_2 = f.read()
     print(poly_2)
 
-print(poly_1.split('+'))
-print(poly_2.split('+'))
+numbers_in_list(poly_1)
+numbers_in_list(poly_2)
+
+# print(poly_1.split('+'))
+# print(poly_2.split('+'))
+
+# print((map(int(poly_1))))
+
+# s = [s for s in str.split(poly_1) if s.isdigit()]
+# print(s)
+# print((poly_1.split('+'))+ (poly_2.split('+')))
+
 
 # sentence = 'Extract 100 , 100.45 and 10000 from this string'
 # s = [int(s) for s in str.split(poly_1) if s.isdigit()]
 # print(s)
 
-import nums_from_string
-
-sentence = 'Extract 100 , 100.45 and 10000 from this string'
-print(nums_from_string.get_nums(sentence))
-
 # import re
 
-# res = re.split('\W+', poly_2) 
+# res = re.split('\W+', poly_2)
 # print(res)
 
 
